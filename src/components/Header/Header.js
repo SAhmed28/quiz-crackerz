55a0e6e664a6d239046e63b34588ebbeef6e81ef
SAhmed28 <sanjida1@ualberta.ca>
2022-10-11 05:59:43 -0600

    Header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb, faPaperPlane, faRuler, faCertificate } from '@fortawesome/free-solid-svg-icons'
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h2 className='text-2xl'>This is Header</h2>
+        <div className='mx-8 flex flex-col md:flex-row'>
+            <div className="header-info">
+                <h1 className='text-3xl font-bold pb-2'>Learn Anything</h1>
+                <p className='text-xl font-semibold'>Get Access for award winning online courses</p>
+                <ul className='md:mt-6 font-semibold'>
+                    <li><FontAwesomeIcon className='md:pr-2 text-cyan-500' icon={faLightbulb} />Creative Study Pattern</li>
+                    <li><FontAwesomeIcon className='md:pr-2 text-cyan-500' icon={faPaperPlane} />Quick Crash Courses</li>
+                    <li><FontAwesomeIcon className='md:pr-2 text-cyan-500' icon={faRuler} />Provided with Experimental Examples</li>
+                    <li><FontAwesomeIcon className='md:pr-2 text-cyan-500' icon={faCertificate} />Certification Awarded</li>
+                </ul>
+            </div>
+            <div className="header-img">
+                <img src="https://miro.medium.com/max/1272/1*ZSVmWGcc1weENb0ShawWxw.gif" alt="header-image" />
+            </div>
         </div>
     );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
     return (
         <nav className='md:flex md:justify-between md:items-center md:m-4 md:p-4 text-center'>
             <div className="title">
-                <h1 className='font-bold md:text-3xl text-2xl text-blue-700'><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600'>Quizio</span> - An Online Learning Platform</h1>
+                <h1 className='font-bold md:text-4xl text-2xl text-cyan-500'><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600'>Quizio</span> - eLearning Platform</h1>
             </div>
             <div className="font-semibold flex flex-col py-2 md:flex md:flex-row md:text-2xl link text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
                 <NavLink className="md:px-4 py-1" to='/'>Home</NavLink>
-                <NavLink className="md:px-4 py-1" to='/statistics'>Statistics</NavLink>
                 <NavLink className="md:px-4 py-1" to='/topics'>Topics</NavLink>
+                <NavLink className="md:px-4 py-1" to='/statistics'>Statistics</NavLink>
                 <NavLink className="md:px-4 py-1" to='/blog'>Blog</NavLink>
             </div>
         </nav>

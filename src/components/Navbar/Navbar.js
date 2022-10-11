b750d6b0efa5eb1b2ffeffe1f7c4f75f89b88f7c
SAhmed28 <sanjida1@ualberta.ca>
2022-10-11 00:48:28 -0600

    Navbar done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,11 +3,16 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/topics'>Topics</NavLink>
-            <NavLink to='/statistics'>Statistics</NavLink>
-            <NavLink to='/blog'>Blog</NavLink>
+        <nav className='md:flex md:justify-between md:items-center md:m-4 md:p-4 text-center'>
+            <div className="title">
+                <h1 className='font-bold md:text-3xl text-2xl text-blue-700'><span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600'>Quizio</span> - An Online Learning Platform</h1>
+            </div>
+            <div className="font-semibold flex flex-col py-2 md:flex md:flex-row md:text-2xl link text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-600">
+                <NavLink className="md:px-4 py-1" to='/'>Home</NavLink>
+                <NavLink className="md:px-4 py-1" to='/statistics'>Statistics</NavLink>
+                <NavLink className="md:px-4 py-1" to='/topics'>Topics</NavLink>
+                <NavLink className="md:px-4 py-1" to='/blog'>Blog</NavLink>
+            </div>
         </nav>
     );
 };

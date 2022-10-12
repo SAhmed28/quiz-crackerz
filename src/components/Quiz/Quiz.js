f7a141c19989cd8e8f4ddfba0cecfa51e665f74b
SAhmed28 <sanjida1@ualberta.ca>
2022-10-11 20:57:48 -0600

    eye icon functional

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 import './Quiz.css'
 
 const Quiz = ({quiz}) => {
-    const {question, options, id} = quiz;
+    const {question, options, id, correctAnswer} = quiz;
     // console.log(quiz)
     const [correct, setCorrect] = useState(0);
     const [wrong, setWrong] = useState(0);
@@ -28,21 +30,41 @@ const Quiz = ({quiz}) => {
         }
         
     }
+
+    let [answer, setAnswer] = useState('');
+    const handleEye = (id) => {
+        console.log('clicked!, id: ', id);
+
+        if(quiz.id === id){
+            console.log(quiz.correctAnswer);
+            answer = quiz.correctAnswer;
+            setAnswer(answer);
+
+            toast.info(answer, {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
+        
+    }
     
     
     return (
         <div className='quiz w-3/4 md:w-1/2 mx-auto p-5 border-solid border-4 border-purple-600 mb-8 rounded-lg bg-white'>
-            <h2 className='text-xl md:text-2xl font-bold text-center pb-8'>Question: {question.slice (3, -4)}</h2>
+            <div className="question-part flex">
+                <h2 className='text-xl md:text-2xl font-bold text-center pb-8'>Question: {question.slice (3, -4)}</h2>
+                <FontAwesomeIcon onClick={() => handleEye(id)} className='text-2xl text-purple-500' icon={faEye} /> <ToastContainer />
+            </div>
             <div className="flex flex-col font-semibold">
                 {
                     options.map(option => 
-                        <label for={option} className='py-3 px-4 text-xl border-solid border-2 mb-3 '>
+                        <label for={option} className='py-3 px-4 text-xl border-solid border-2 mb-3 flex'>
                             <input onClick={()=>handleCorrectAnsCount(id, option)} type="radio" id={option} name={id} value={option}/> <ToastContainer />
                                 <span className='pl-3'>{option}</span>
                         </label>
                     )
 
                 }
+                
             </div>
         </div>
     );
